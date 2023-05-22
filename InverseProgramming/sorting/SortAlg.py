8b7e99f57dae9445161c9cdbe2ed8f893934d3e5
grotty <gateway07@outlook.com>
2023-05-22 11:49:25 +0300

    Sorting algorithms based on Z3

---
+++ InverseProgramming/sorting/SortAlg.py
@@ -0,0 +1,161 @@
+from typing import List
+
+from z3 import *
+
+from Z3Util import get_models
+
+
+def sort_dot(n: int) -> (BoolRef, List, List):
+    """ Define constraints for array's sorting based on idea that [I] * [M] = [O], where I - input vector, M -
+    permutation matrix with 0 and 1, O - output sorted vector, * - dot vector's operation. """
+    in_vec = [Int('x{}'.format(i)) for i in range(n)]
+
+    matrix = []
+    for row in range(n):
+        # A for loop for column entries
+        matrix.append([Bool('{}_{}'.format(col, row)) for col in range(n)])
+    f = []
+    for i in range(n):
+        l1 = [(row, 1) for row in matrix[i]]
+        l2 = [(col[i], 1) for col in matrix]
+        f.append(And(PbEq(l1, 1), PbEq(l2, 1)))
+
+    out_vec = []
+    for i in range(n):
+        s = [If(matrix[i][j], in_vec[j], 0) for j in range(n)]
+        out_vec.append(Sum(s))
+
+    for i in range(n - 1):
+        f.append(out_vec[i] <= out_vec[i + 1])
+
+    return And(f), in_vec, out_vec
+
+
+def sort_bubble(n: int) -> (BoolRef, List, List):
+    """ Define constraints for array's sorting based on classical bubble sort.  """
+
+    def up(arr):
+        fs = []
+        for i in range(len(arr) - 1):
+            x = arr[i]
+            y = arr[i + 1]
+            # compare and swap x and y
+            x1, y1 = FreshInt(), FreshInt()
+
+            c = If(x <= y, And(x1 == x, y1 == y), And(x1 == y, y1 == x))
+            # store values
+            arr[i] = x1
+            arr[i + 1] = y1
+            fs.append(c)
+        return fs
+
+    fs = []  # list of assertions to be returned
+    in_vec = [Int('x{}'.format(i)) for i in range(n)]
+
+    # recursive call to bubble_up
+    out_vec = in_vec.copy()
+    for _ in range(n):
+        cst = up(out_vec)
+        fs.extend(cst)
+    return And(fs), in_vec, out_vec
+
+
+def sort_merge(n: int) -> (BoolRef, List, List):
+    """ Define constraints for array's sorting based on classical sort by merging.  """
+
+    def cmp2(x, y):
+        l, g = FreshInt(), FreshInt()
+        return [If(x <= y, And(l == x, g == y), And(l == y, g == x))], [l, g]
+
+    def cmp3(x, y, z) -> (List, List):
+        f1, (l0, g0) = cmp2(x, y)
+        f2, (l1, g1) = cmp2(g0, z)
+        f3, (l2, g2) = cmp2(l0, l1)
+        return [f1[0], f2[0], f3[0]], [l2, g2, g1]
+
+    def merge(xs: List, ys: List) -> (List, List):
+        assert len(xs) == len(ys) or len(xs) == len(ys) - 1
+
+        fs, lower_vars, greater_vars, ls0, gs0 = [], [], [], xs.copy(), ys.copy()
+        while len(ls0) > 0:
+            ls1, gs1 = [], []
+            for i in range(len(ls0)):
+                f, (l, g) = cmp2(ls0[i], gs0[i])
+                fs.extend(f)
+                ls1.append(l)
+                gs1.append(g)
+
+            n1, n2 = len(ls0), len(gs0)
+            ls0 = ls1[1:]
+            if n1 == n2 - 1:
+                ls0.append(gs0[n2 - 1])
+                gs0 = gs1
+            else:
+                greater_vars.append(gs1[-1])
+                gs0 = gs1[:-1]
+            lower_vars.append(ls1[0])
+
+        assert len(lower_vars) + len(greater_vars) == len(xs) + len(ys)
+        lower_vars.extend(reversed(greater_vars))
+        return fs, lower_vars
+
+    def split(xs: List) -> (List, List):
+        n = len(xs)
+        assert n > 1
+        if n == 2:
+            return cmp2(xs[0], xs[1])
+        elif n == 3:
+            return cmp3(xs[0], xs[1], xs[2])
+
+        m = int(n / 2)
+        f1, left = split(xs[:m])
+        f2, right = split(xs[m:])
+        f3, out_list = merge(left, right)
+        assert len(out_list) == n
+
+        fs = []
+        fs.extend(f1)
+        fs.extend(f2)
+        fs.extend(f3)
+
+        return fs, out_list
+
+    in_vec = [Int('{}'.format(i)) for i in range(n)]
+
+    fs, out_vec = split(in_vec)
+    fs.extend([out_vec[i] <= out_vec[i + 1] for i in range(n - 1)])
+    return And(fs), in_vec, out_vec
+
+
+def sort_index(n: int) -> (BoolRef, List, List):
+    """ Define constraints for array's sorting based on invariant that sorting value as index and its counts
+    are the same before and after sorting!
+    """
+    in_vec = [FreshInt() for _ in range(n)]
+    in_counts = K(IntSort(), 0)
+    for v in in_vec:
+        in_counts = Store(in_counts, v, in_counts[v] + 1)
+
+    out_vec = [FreshInt() for _ in range(n)]
+    out_counts = K(IntSort(), 0)
+    for v in out_vec:
+        out_counts = Store(out_counts, v, out_counts[v] + 1)
+
+    fs = []
+    for i in range(n - 1):
+        fs.append(out_vec[i] <= out_vec[i + 1])
+
+    for e in out_vec:
+        fs.append(in_counts[e] == out_counts[e])
+    return And(fs), in_vec, out_vec
+
+
+if __name__ == "__main__":
+    vec = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 10, 9, 8, 7, -6, 5, 4, 3, 2, 1, 23, 43, 11, 1]
+    n = len(vec)
+    fs, in_vec, out_vec = sort_merge(n)  # _merge / _bubble / _dot / _index
+
+    # check = [And(in_vec[j] == vec[j]) for j in range(n)]
+    check = []
+    for m in get_models(And(fs, And(check)), out_vec):
+        print([m.eval(out_vec[j]) for j in range(n)])
---
+++ InverseProgramming/sorting/Z3Util.py
@@ -0,0 +1,40 @@
+from time import perf_counter
+from typing import List
+
+from z3 import *
+
+
+def to_smt2_string(f, status="unknown", name="", logic=""):
+    v = (Ast * 0)()
+    return Z3_benchmark_to_smtlib_string(f.ctx_ref(), name, logic, status, "", 0, v, f.as_ast())
+
+
+def get_models(F: BoolRef, var_refs: List[BoolRef], verbose: bool = False):
+    s = Solver()
+    s.set(mbqi=True)
+    s.set(pull_nested_quantifiers=True)
+
+    s.add(F)
+    if verbose:
+        print(s.sexpr())
+        print(s.check())
+
+    result = []
+    time = perf_counter()
+    while s.check() == sat:
+        model = s.model()
+        print("Time:", perf_counter() - time)
+        if verbose:
+            # print(sorted([(d, model[d]) for d in model], key=lambda x: str(x[0])))
+            for d in model:
+                print("%s -> %s" % (d, model[d]))
+
+        result.append(model)
+
+        block = []
+        for var_ref in var_refs:
+            block.append(var_ref != model.eval(var_ref, model_completion=True))
+
+        s.add(Or(block))
+        time = perf_counter()
+        yield model
